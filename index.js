94270d83e9d9382c360e6a6be1f1a335b7819164
Esteban Santos <santosesteban09@gmail.com>
2021-07-02 23:40:17 -0500

    I have the license working now, link and badge. I just need to make some format adjustments. Otherwise it looks to be finished!

--- index.js
+++ index.js
@@ -2,17 +2,6 @@
 let comm = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
-
 comm.prompt([
     {
         type: 'input',
@@ -52,7 +41,7 @@ comm.prompt([
     {
 
         type: 'input',
-        message: "Who is/are the contributors to this project? What are the guidelines for contribution?",
+        message: "Who is/are the contributors to this project?",
         name: 'contributors',
     },
     {
@@ -72,7 +61,7 @@ comm.prompt([
         type: 'list',
         message: "Please select a license from the following list:",
         name: 'license',
-        choices: ["Apache", "Boost", "BSD3", "BSD2", "Eclipse", "GPLv3", "GPLv2", "AGPLv3", "LGPlv3","FDL","IBM","ISC","MIT","MPL", "PERL","Artistic","Unlicense"],
+        choices: ["Apache", "Boost", "BSD3", "BSD2", "Eclipse", "GPLv3", "GPLv2", "AGPLv3", "LGPlv3","FDL","IBM","ISC","MIT","MPL", "PERL","Artistic","Unlicense", "None"],
     },
     {
         type: 'checkbox',
@@ -80,19 +69,6 @@ comm.prompt([
         name: 'stack',
         choices: ["HTML", "CSS", "Javascript", "node.js", "react", "SQL"],
     },
-    // {
-    //     type:'checkbox',
-    //         message:"What languages do you know?",
-    //         name: 'stack',
-    //         choices: ["HTML","CSS","JAVASCRIPT","MySQL"],
-    // },
-    // {
-    //     type:'list',
-    //         message:"What is your preferred method of communication?",
-    //         name: 'communication',        
-    //         choices: ["email", "text", "phone"],
-    // }
-
 
 ])
     // function to write README file
@@ -100,11 +76,123 @@ comm.prompt([
         let license = response.license.toLowerCase();
         let badge = '';
         let link = '';
-        renderLicenseBadge(license);
-        renderLicenseLink(license);
+        const languages = response.stack;
+        // renderLicenseBadge(license);
+        // renderLicenseLink(license);
+        switch (license){
+            case "apache":
+              badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+              break;
+            case "boost":
+              badge = "![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)";
+              break;
+            case "bsd3":
+              badge = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
+              break;
+            case "bsd2":
+              badge = "![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)";
+              break;
+            case "eclipse":
+              badge = "![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)";
+              break;
+            case "gplv3":
+              badge =  "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+              break;
+            case "gplv2":
+              badge = "![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)";
+              break;
+            case "agplv3":
+              badge = "![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)";
+              break;
+            case "lgplv3":
+              badge = "![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)";
+              break;
+            case "fdl":
+              badge = "![License: FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)";
+              break;
+            case "ibm":
+              badge = "![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)";
+              break;
+            case "isc":
+              badge = "![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)";
+              break;
+            case "mit":
+              badge = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+              break;
+            case "mpl":
+              badge = "![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)";
+              break;
+            case "perl":
+              badge = "![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)";
+              break;
+            case "artistic":
+              badge = "![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)";
+              break;
+            case "unlicense":
+              badge = "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)";
+              break;
+            default:
+              badge = '';
+          };
+          switch (license){
+            case "apache":
+              link = 'https://opensource.org/licenses/Apache-2.0';
+              break;
+            case "boost":
+              link = "https://www.boost.org/LICENSE_1_0.txt";
+              break;
+            case "bsd3":
+              link = "https://opensource.org/licenses/BSD-3-Clause";
+              break;
+            case "bsd2":
+              link = "https://opensource.org/licenses/BSD-2-Clause";
+              break;
+            case "eclipse":
+              link = "https://opensource.org/licenses/EPL-1.0";
+              break;
+            case "gplv3":
+              link =  "https://www.gnu.org/licenses/gpl-3.0";
+              break;
+            case "gplv2":
+              link = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html";
+              break;
+            case "agplv3":
+              link = "https://www.gnu.org/licenses/agpl-3.0";
+              break;
+            case "lgplv3":
+              link = "https://www.gnu.org/licenses/lgpl-3.0";
+              break;
+            case "fdl":
+              link = "https://www.gnu.org/licenses/fdl-1.3";
+              break;
+            case "ibm":
+              link = "https://opensource.org/licenses/IPL-1.0";
+              break;
+            case "isc":
+              link = "https://opensource.org/licenses/ISC";
+              break;
+            case "mit":
+              link = "https://opensource.org/licenses/MIT";
+              break;
+            case "mpl":
+              link = "https://opensource.org/licenses/MPL-2.0";
+              break;
+            case "perl":
+              link = "https://opensource.org/licenses/Artistic-2.0";
+              break;
+            case "artistic":
+              link = "https://opensource.org/licenses/Artistic-2.0";
+              break;
+            case "unlicense":
+              link = "http://unlicense.org/";
+              break;
+            default:
+              link = '';
+          };
+
+
         const GeneratedReadme = `
-    # ${response.title}
-        ${badge};
+  #  ${response.title} ${badge}
 
   ## Description:
 
@@ -138,12 +226,15 @@ comm.prompt([
   ## License:
   This project is covered under the following license. See link for more information.
   If empty, no license is being used.
+    
     ${response.license}
     ${link}
 
   ## Contributors: 
-    ${response.contributors}
-    If you would like to contribute please follow:
+    ${response.contributors.split(' ').join(', ')}
+
+  If you would like to contribute please follow:
+
     ${response.guidelines}
 
   ## Test Instructions:
@@ -162,7 +253,8 @@ Screenshot will be found here
 
   
   ## Technologies-Used:
- ${response.stack}
+  
+    ${response.stack}
 
 
 ## Any questions/suggestions/critique?
@@ -195,119 +287,119 @@ ${response.email}
     function renderLicenseBadge(license) {
         switch (license){
           case "apache":
-            badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
             break;
           case "boost":
-            badge = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+            badge = "![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)";
             break;
           case "bsd3":
-            badge = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+            badge = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
             break;
           case "bsd2":
-            badge = "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+            badge = "![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)";
             break;
           case "eclipse":
-            badge = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+            badge = "![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)";
             break;
           case "gplv3":
-            badge =  "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+            badge =  "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
             break;
           case "gplv2":
-            badge = "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+            badge = "![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)";
             break;
           case "agplv3":
-            badge = "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+            badge = "![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)";
             break;
           case "lgplv3":
-            badge = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+            badge = "![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)";
             break;
           case "fdl":
-            badge = "[![License: FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)";
+            badge = "![License: FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)";
             break;
           case "ibm":
-            badge = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+            badge = "![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)";
             break;
           case "isc":
-            badge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+            badge = "![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)";
             break;
           case "mit":
-            badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            badge = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
             break;
           case "mpl":
-            badge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+            badge = "![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)";
             break;
           case "perl":
-            badge = "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+            badge = "![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)";
             break;
           case "artistic":
-            badge = "[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+            badge = "![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)";
             break;
           case "unlicense":
-            badge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+            badge = "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)";
             break;
           default:
             badge = '';
         }
-
+        return badge;
 }
 
 
 function renderLicenseLink(license) {
     switch (license){
       case "apache":
-        link = '(https://opensource.org/licenses/Apache-2.0)';
+        link = 'https://opensource.org/licenses/Apache-2.0';
         break;
       case "boost":
-        link = "(https://www.boost.org/LICENSE_1_0.txt)";
+        link = "https://www.boost.org/LICENSE_1_0.txt";
         break;
       case "bsd3":
-        link = "(https://opensource.org/licenses/BSD-3-Clause)";
+        link = "https://opensource.org/licenses/BSD-3-Clause";
         break;
       case "bsd2":
-        link = "(https://opensource.org/licenses/BSD-2-Clause)";
+        link = "https://opensource.org/licenses/BSD-2-Clause";
         break;
       case "eclipse":
-        link = "(https://opensource.org/licenses/EPL-1.0)";
+        link = "https://opensource.org/licenses/EPL-1.0";
         break;
       case "gplv3":
-        link =  "(https://www.gnu.org/licenses/gpl-3.0)";
+        link =  "https://www.gnu.org/licenses/gpl-3.0";
         break;
       case "gplv2":
-        link = "(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+        link = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html";
         break;
       case "agplv3":
-        link = "(https://www.gnu.org/licenses/agpl-3.0)";
+        link = "https://www.gnu.org/licenses/agpl-3.0";
         break;
       case "lgplv3":
-        link = "(https://www.gnu.org/licenses/lgpl-3.0)";
+        link = "https://www.gnu.org/licenses/lgpl-3.0";
         break;
       case "fdl":
-        link = "(https://www.gnu.org/licenses/fdl-1.3)";
+        link = "https://www.gnu.org/licenses/fdl-1.3";
         break;
       case "ibm":
-        link = "(https://opensource.org/licenses/IPL-1.0)";
+        link = "https://opensource.org/licenses/IPL-1.0";
         break;
       case "isc":
-        link = "(https://opensource.org/licenses/ISC)";
+        link = "https://opensource.org/licenses/ISC";
         break;
       case "mit":
-        link = "(https://opensource.org/licenses/MIT)";
+        link = "https://opensource.org/licenses/MIT";
         break;
       case "mpl":
-        link = "(https://opensource.org/licenses/MPL-2.0)";
+        link = "https://opensource.org/licenses/MPL-2.0";
         break;
       case "perl":
-        link = "(https://opensource.org/licenses/Artistic-2.0)";
+        link = "https://opensource.org/licenses/Artistic-2.0";
         break;
       case "artistic":
-        link = "(https://opensource.org/licenses/Artistic-2.0)";
+        link = "https://opensource.org/licenses/Artistic-2.0";
         break;
       case "unlicense":
-        link = "(http://unlicense.org/)";
+        link = "http://unlicense.org/";
         break;
       default:
         link = '';
     }
-  
+  return link;
   
   }
